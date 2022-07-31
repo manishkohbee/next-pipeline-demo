874919acdd5cc53e08aea2ed1be5d87e94443058
manishkohbee <manish@kohbee.com>
2022-07-31 19:21:26 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,275 @@
+/* eslint-disable @next/next/no-img-element */
+
+export default function Home() {
+  return (
+    <>
+
+      <section className="relative pb-56 sm:pb-80">
+        <img className="hidden lg:block absolute inset-0 w-full" src="atis-assets/background/lines.svg" alt="" />
+        <div className="relative pt-12 md:pt-16 pb-32 md:pb-64 bg-gray-50 border-b-4 border-green-600">
+          <div className="relative container mx-auto px-4">
+            <div className="max-w-5xl mx-auto text-center mb-12 md:mb-20">
+              <h2 className="mb-10 text-4xl lg:text-5xl font-bold">Build Your Lead Generation Machine Using The Power Of LinkedIn In Just 5 Days!</h2>
+              <h4 className="mb-10 text-2xl lg:text-3xl">Secrets that generated me over 300K+ followers, and 1000s of leads for FREE bringing in thousands of dollars on a monthly basis from Linkedin.</h4>
+            </div>
+
+
+            <section className="py-24 bg-gray-100 max-w-5xl mx-auto">
+              <div className="flex flex-wrap -mx-4 -mb-4 md:mb-0">
+                <div className="w lg:w-1/2 flex flex-wrap -mx-4 -mb-8">
+                  <div className="w-1/2 px-4 mb-8">
+                    <div className="p-4 lg:p-6 bg-green-100">
+                      <img className="mb-6" src="pstls-assets/icons/rounded-document-clean.svg" alt="" />
+                      <h5 className="text-xl font-heading mb-4">Title 1</h5>
+                    </div>
+                  </div>
+                  <div className="w-1/2 px-4 mb-8">
+                    <div className="p-4 lg:p-6 bg-green-100">
+                      <img className="mb-6" src="pstls-assets/icons/rounded-filters.svg" alt="" />
+                      <h5 className="text-xl font-heading mb-4">Title 2</h5>
+                    </div>
+                  </div>
+                  <div className="w-1/2 px-4 mb-8">
+                    <div className="p-4 lg:p-6 bg-green-100">
+                      <img className="mb-6" src="pstls-assets/icons/rounded-document-clean.svg" alt="" />
+                      <h5 className="text-xl font-heading mb-4">Title 1</h5>
+                    </div>
+                  </div>
+                  <div className="w-1/2 px-4 mb-8">
+                    <div className="p-4 lg:p-6 bg-green-100">
+                      <img className="mb-6" src="pstls-assets/icons/rounded-filters.svg" alt="" />
+                      <h5 className="text-xl font-heading mb-4">Title 2</h5>
+                    </div>
+                  </div>
+                  <div className="w-1/2 px-4 mb-8">
+                    <div className="p-4 lg:p-6 bg-green-100">
+                      <img className="mb-6" src="pstls-assets/icons/rounded-document-clean.svg" alt="" />
+                      <h5 className="text-xl font-heading mb-4">Title 1</h5>
+                    </div>
+                  </div>
+                  <div className="w-1/2 px-4 mb-8">
+                    <div className="p-4 lg:p-6 bg-green-100">
+                      <img className="mb-6" src="pstls-assets/icons/rounded-filters.svg" alt="" />
+                      <h5 className="text-xl font-heading mb-4">Title 2</h5>
+                    </div>
+                  </div>
+                </div>
+                <div className="w lg:w-1/2 flex flex-wrap -mx-4 -mb-8">
+                  <div className="max-w px-4 my-8 mx-8 lg:my-0">
+                    <div className="p-4 lg:p-6 bg-green-100">
+                      <img className="mb-6" src="pstls-assets/icons/rounded-document-clean.svg" alt="" />
+                      <h5 className="text-xl p-40 font-heading mb-4">Big Tile</h5>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full md:w-1/2 px-4 mb-4 md:mb-0"></div>
+              </div>
+            </section>
+          </div>
+
+        </div>
+      </section>
+
+      <section className="py-24">
+        <div className="container px-4 mx-auto">
+          <p className="leading-8 text-gray-500 text-center mb-12">Trusted by brands all over the world</p>
+          <div className="flex flex-wrap -mx-4 items-center">
+            <div className="w-full md:w-1/2 lg:w-1/4 px-4 text-center mb-10 lg:mb-0">
+              <div className="p-4 h-40 flex items-center justify-center bg-indigo-100">
+                <img src="pstls-assets/logos/coca-cola-b-xl.svg" alt="" />
+              </div>
+            </div>
+            <div className="w-full md:w-1/2 lg:w-1/4 px-4 text-center mb-10 lg:mb-0">
+              <div className="p-4 h-40 flex items-center justify-center bg-pink-100">
+                <img src="pstls-assets/logos/apple-b-xl.svg" alt="" />
+              </div>
+            </div>
+            <div className="w-full md:w-1/2 lg:w-1/4 px-4 text-center mb-10 md:mb-0">
+              <div className="p-4 h-40 flex items-center justify-center bg-orange-100">
+                <img src="pstls-assets/logos/champion-b-xl.svg" alt="" />
+              </div>
+            </div>
+            <div className="w-full md:w-1/2 lg:w-1/4 px-4 text-center">
+              <div className="p-4 h-40 flex items-center justify-center bg-lime-100">
+                <img src="pstls-assets/logos/sony-b-xl.svg" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="py-24 bg-gray-100">
+        <div className="container px-4 mx-auto">
+          <div className="max-w-xl mx-auto mb-10 text-center">
+            <h2 className="text-5xl font-heading mb-6 ">Features List</h2>
+            <p className="leading-8 ">This UI Kit uses attractive colors and modern typography to make you look good, no matter what business you’re in.</p>
+          </div>
+          <div className="flex flex-wrap -mx-4 -mb-10">
+            <div className="w-full md:w-1/2 lg:w-1/4 px-4 mb-10">
+              <div className="max-w-xs mx-auto text-center">
+                <img className="block mx-auto mb-8" src="pstls-assets/icons/features-2b-document-clean.svg" alt="" />
+                <h5 className="text-xl font-heading mb-4 ">Layer styles</h5>
+                <p className="leading-8 text-gray-400">Take care to develop resources continually and integrity them with previous projects.</p>
+              </div>
+            </div>
+            <div className="w-full md:w-1/2 lg:w-1/4 px-4 mb-10">
+              <div className="max-w-xs mx-auto text-center">
+                <img className="block mx-auto mb-8" src="pstls-assets/icons/features-2b-filters-1.svg" alt="" />
+                <h5 className="text-xl font-heading mb-4 ">Change of access</h5>
+                <p className="leading-8 text-gray-400">Take care to develop resources continually and integrity them with previous projects.</p>
+              </div>
+            </div>
+            <div className="w-full md:w-1/2 lg:w-1/4 px-4 mb-10">
+              <div className="max-w-xs mx-auto text-center">
+                <img className="block mx-auto mb-8" src="pstls-assets/icons/features-2b-users.svg" alt="" />
+                <h5 className="text-xl font-heading mb-4 ">List of regular users</h5>
+                <p className="leading-8 text-gray-400">Take care to develop resources continually and integrity them with previous projects.</p>
+              </div>
+            </div>
+            <div className="w-full md:w-1/2 lg:w-1/4 px-4 mb-10">
+              <div className="max-w-xs mx-auto text-center">
+                <img className="block mx-auto mb-8" src="pstls-assets/icons/features-2b-check-circle.svg" alt="" />
+                <h5 className="text-xl font-heading mb-4 ">No errors</h5>
+                <p className="leading-8 text-gray-400">Take care to develop resources continually and integrity them with previous projects.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="py-24">
+          <div className="container px-4 mx-auto">
+            <div className="max-w-2xl mx-auto text-center">
+              <h2 className="text-4xl md:text-5xl font-heading mb-6">The latest version of this excellent product is waiting to deploy.</h2>
+              <p className="max-w-md lg:max-w-xl leading-8 mx-auto mb-6">A short message that will bring your customers into the world of your imagination. And will allow them to become more familiar with your brand.</p>
+              <a className="inline-block w-full md:w-auto py-3 px-8 text-center text-black font-bold bg-indigo-200 hover:bg-indigo-300 transition duration-200" href="#">Activate Demo</a>
+            </div>
+          </div>
+        </div>
+        <div className="pt-16 pb-24 bg-black">
+          <div className="container px-4 mx-auto">
+            <div className="flex flex-wrap -mx-4">
+              <div className="w-full md:w-1/3 px-4 mb-12 md:mb-0">
+                <div className="max-w-xs md:px-10 mx-auto text-center">
+                  <img className="block mx-auto mb-6" src="pstls-assets/icons/call-to-action-3b-users.svg" alt="" />
+                  <h5 className="text-lg font-heading mb-2 text-white">Spectacular team plan</h5>
+                  <p className="leading-8 text-white">Fairly assigning daily tasks to your employees</p>
+                </div>
+              </div>
+              <div className="w-full md:w-1/3 px-4 mb-12 md:mb-0">
+                <div className="max-w-xs md:px-10 mx-auto text-center">
+                  <img className="block mx-auto mb-6" src="pstls-assets/icons/call-to-action-3b-sharable.svg" alt="" />
+                  <h5 className="text-lg font-heading mb-2 text-white">Sharable showcase</h5>
+                  <p className="leading-8 text-white">Team members will be up to date on the project</p>
+                </div>
+              </div>
+              <div className="w-full md:w-1/3 px-4">
+                <div className="max-w-xs md:px-10 mx-auto text-center">
+                  <img className="block mx-auto mb-6" src="pstls-assets/icons/call-to-action-3b-new-messages.svg" alt="" />
+                  <h5 className="text-lg font-heading mb-2 text-white">Generate new messages</h5>
+                  <p className="leading-8 text-white">More interesting writings for your customers</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="py-24">
+        <div className="container px-4 mx-auto">
+          <div className="max-w-xl mx-auto mb-12 text-center">
+            <h2 className="text-4xl md:text-5xl font-heading mb-6">Testimonials</h2>
+            <p className="leading-8">This UI Kit uses attractive colors and modern typography to make you look good, no matter what business you’re in.</p>
+          </div>
+          <div className="flex flex-wrap -mx-4 -mb-8">
+            <div className="w-full md:w-1/2 lg:w-1/4 px-4 mb-8">
+              <div className="max-w-xs mx-auto py-8 px-6 lg:px-10">
+                <img className="mb-3" src="pstls-assets/images/testimonials/testimonials-3-photo-circle-2.png" alt="" />
+                <p className="text-xl font-heading mb-2">John Doe</p>
+                <span className="text-gray-500">Front-end developer</span>
+                <p className="leading-8 text-gray-500 mt-6">Many people were hoping that if the Democrats won control of Congress they would reverse the online</p>
+              </div>
+            </div>
+            <div className="w-full md:w-1/2 lg:w-1/4 px-4 mb-8">
+              <div className="max-w-xs mx-auto py-8 px-6 lg:px-10 bg-indigo-100">
+                <img className="mb-3" src="pstls-assets/images/testimonials/testimonials-3-photo-circle.png" alt="" />
+                <p className="text-xl font-heading mb-2">Sophie Kovalsky</p>
+                <span className="text-gray-500">UI/UX Designer</span>
+                <p className="leading-8 mt-6 mb-6">Many people were hoping that if the Democrats won control of Congress they would reverse the online</p>
+                <div className="flex items-center">
+                  <a className="inline-block mr-2" href="#">
+                    <img src="pstls-assets/logos/twitter.svg" alt="" />
+                  </a>
+                  <a className="inline-block mr-2" href="#">
+                    <img src="pstls-assets/logos/google.svg" alt="" />
+                  </a>
+                  <a className="inline-block" href="#">
+                    <img src="pstls-assets/logos/facebook.svg" alt="" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="w-full md:w-1/2 lg:w-1/4 px-4 mb-8">
+              <div className="max-w-xs mx-auto py-8 px-6 lg:px-10">
+                <img className="mb-3" src="pstls-assets/images/testimonials/testimonials-3-photo-circle-3.png" alt="" />
+                <p className="text-xl font-heading mb-2">Sophie Conor</p>
+                <span className="text-gray-500">Project Manager</span>
+                <p className="leading-8 text-gray-500 mt-6 mb-6">Many people were hoping that if the Democrats won control of Congress they would reverse the online</p>
+              </div>
+            </div>
+            <div className="w-full md:w-1/2 lg:w-1/4 px-4 mb-8">
+              <div className="max-w-xs mx-auto py-8 px-6 lg:px-10">
+                <img className="mb-3" src="pstls-assets/images/testimonials/testimonials-3-photo-circle-4.png" alt="" />
+                <p className="text-xl font-heading mb-2">David Smith</p>
+                <span className="text-gray-500">Researcher</span>
+                <p className="leading-8 text-gray-500 mt-6 mb-6">Many people were hoping that if the Democrats won control of Congress they would reverse the online</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="py-24 relative">
+        <img className="absolute top-0 left-0" src="pstls-assets/images/contacts/shadow-small.png" alt="" />
+        <div className="container px-4 mx-auto relative">
+          <div className="flex flex-wrap -mx-4 items-center">
+            <div className="w-full lg:w-1/2 px-4 mb-14 lg:mb-0">
+              <div className="max-w-md">
+                <h3 className="mb-5 text-xl md:text-2xl font-heading text-indigo-200">Get in touch with us</h3>
+                <h2 className="text-4xl md:text-5xl lg:text-6xl font-heading">Is the answer to your question missing?</h2>
+              </div>
+            </div>
+            <div className="w-full lg:w-1/2 px-4">
+              <div className="flex flex-wrap -mx-4">
+                <div className="w-full md:w-1/2 px-4 mb-10">
+                  <p className="text-gray-500">E-mail</p>
+                  <a className="inline-block text-black hover:underline" href="mailto:contact@pstls.com">contact@pstls.com</a>
+                </div>
+                <div className="w-full md:w-1/2 px-4 mb-10">
+                  <p className="text-gray-500">Phone</p>
+                  <p>+1-2020-555-109</p>
+                </div>
+                <div className="w-full px-4">
+                  <p className="text-gray-500 mb-2">Socials</p>
+                  <div>
+                    <a className="inline-block" href="#">
+                      <img src="pstls-assets/images/contacts/facebook.png" alt="" />
+                    </a>
+                    <a className="inline-block" href="#">
+                      <img src="pstls-assets/images/contacts/twitter.png" alt="" />
+                    </a>
+                    <a className="inline-block" href="#">
+                      <img src="pstls-assets/images/contacts/google.png" alt="" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </>
+  )
+}
